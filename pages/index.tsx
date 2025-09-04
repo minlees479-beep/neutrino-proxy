a9a9b69f5f7adcd71f579e1b86400e2ac505ca38
minlees479-beep <minlees479@gmail.com>
2025-09-04 17:28:09 +0800

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>Neutrino Proxy 正常运行 🎉</h1>
+      <p>API 地址： <code>/api/lookup</code></p>
+    </div>
+  )
+}
